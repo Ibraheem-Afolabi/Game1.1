17a8836206e469ed6fdf5acf2cf8028e1a9a3019
Ibraheem-Afolabi <saduibraheem@gmail.com>
2023-12-25 00:46:23 +0100

    Added another spritesheet.
    Added the movement code for another spritesheet. There are two spritesheets now both with different buttons for movement.

--- Game1.1/main.cpp
+++ Game1.1/main.cpp
@@ -13,18 +13,25 @@ int main() {
 	//--------------- INITIALIZE ----------
 
 	//--------------- LOAD ----------
-	sf::Texture player1texture;
-	sf::Sprite player1sprite;
+	sf::Texture player1texture, enemy1texture;
+	sf::Sprite player1sprite, enemy1sprite;
+
+	//x and y axis, Width and Height (They're integers)
+	int Xindex = 0, Yindex = 0;
 
 	if (player1texture.loadFromFile("Assets/Player/Textures/spritesheet2.png")) {
 		std::cout << "Player1 loaded!\n";
 		player1sprite.setTexture(player1texture);
-		//player1sprite.scale(1, 4);
-
-		//x and y axis, Width and Height (They're integers)
-		int Xindex = 0, Yindex = 0;
+		//player1sprite.scale(1, 4)
 
 		player1sprite.setTextureRect(sf::IntRect(Xindex * 64, Yindex * 64, 64, 64));
+	}
+	if (enemy1texture.loadFromFile("Assets/Player/Textures/spritesheet.png")) {
+		std::cout << "Enemy1 Loaded!\n";
+		enemy1sprite.setTexture(enemy1texture);
+
+		enemy1sprite.setPosition(200, 0);
+		enemy1sprite.setTextureRect(sf::IntRect(Xindex * 64, Yindex * 64, 64, 64));
 	}
 	else {
 		std::cout << "Player image failed to load!\n";
@@ -47,6 +54,7 @@ int main() {
 		}
 	
 		sf::Vector2f oldPosition = player1sprite.getPosition();
+		sf::Vector2f oldPosition1 = enemy1sprite.getPosition();
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))	
 			player1sprite.setPosition(oldPosition + sf::Vector2f(0, -1));
 		
@@ -58,13 +66,23 @@ int main() {
 		
 		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) 
 			player1sprite.setPosition(oldPosition + sf::Vector2f(-1, 0));
-
+		
+		//Movement For Enemy Sprite
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::I))
+			enemy1sprite.setPosition(oldPosition1 + sf::Vector2f(0, -1));
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::J))
+			enemy1sprite.setPosition(oldPosition1 + sf::Vector2f(-1, 0));
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+			enemy1sprite.setPosition(oldPosition1 + sf::Vector2f(1, 0));
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::M))
+			enemy1sprite.setPosition(oldPosition1 + sf::Vector2f(0, 1));
 		//------------ UPDATE -------------
 
 			//------------ DRAW -------------
 			window.clear(sf::Color::Black);
 
 			window.draw(player1sprite);
+			window.draw(enemy1sprite);
 
 			window.display();
 			//------------ DRAW -------------
