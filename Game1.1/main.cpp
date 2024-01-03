dc8a6adba0f1fece91a0143ba14cf2ce0b415402
Ibraheem-Afolabi <saduibraheem@gmail.com>
2024-01-03 14:43:34 +0100

    Code for bullets.
    Added a bullet,
    Added a code for the player to shoot bullets to the enemy.

--- Game1.1/main.cpp
+++ Game1.1/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <math.h>
+
+
+sf::Vector2f NormalizeVector(sf::Vector2f vector) {
+	float m = std::sqrt((vector.x * vector.x) + (vector.y * vector.y));
+
+	sf::Vector2f normalizedVector;
+	normalizedVector.x = vector.x / m;
+	normalizedVector.y = vector.y / m;
+
+	return normalizedVector;
+}
 
 int main() {
+
 	//--------------- INITIALIZE ----------
 
 	//Contex Settings...
@@ -12,32 +26,48 @@ int main() {
 
 	//--------------- INITIALIZE ----------
 
-	//--------------- LOAD ----------
-	sf::Texture player1texture, enemy1texture;
-	sf::Sprite player1sprite, enemy1sprite;
 
-	//x and y axis, Width and Height (They're integers)
-	int Xindex = 0, Yindex = 0;
+
+	//--------------- LOAD ----------
+
+	sf::Texture player1texture, skeletonTexture;
+	sf::Sprite player1sprite, skeletonSprite;
+
+	//----------- Player -----------------------
 
 	if (player1texture.loadFromFile("Assets/Player/Textures/spritesheet2.png")) {
 		std::cout << "Player1 loaded!\n";
 		player1sprite.setTexture(player1texture);
+		player1sprite.setPosition(650, 500);
 		//player1sprite.scale(1, 4)
+
+		int Xindex = 0, Yindex = 2;
 
 		player1sprite.setTextureRect(sf::IntRect(Xindex * 64, Yindex * 64, 64, 64));
 	}
-	if (enemy1texture.loadFromFile("Assets/Player/Textures/spritesheet.png")) {
-		std::cout << "Enemy1 Loaded!\n";
-		enemy1sprite.setTexture(enemy1texture);
+	//----------- Player -----------------------
 
-		enemy1sprite.setPosition(200, 0);
-		enemy1sprite.setTextureRect(sf::IntRect(Xindex * 64, Yindex * 64, 64, 64));
+	std::vector<sf::RectangleShape> Bullets;
+	float bulletSpeed = 0.7f;
+
+	//----------- Enemy -----------------------
+
+	if (skeletonTexture.loadFromFile("Assets/Enemy/Textures/spritesheet.png")) {
+		std::cout << "Skeleton Loaded!\n";
+		skeletonSprite.setTexture(skeletonTexture);
+
+		int Xindex = 0, Yindex = 2;
+
+		skeletonSprite.setPosition(200, 200);
+		skeletonSprite.setTextureRect(sf::IntRect(Xindex * 64, Yindex * 64, 64, 64));
 	}
+	//----------- Enemy -----------------------
 	else {
 		std::cout << "Player image failed to load!\n";
 	}
 	
 	//--------------- LOAD ----------
+
 
 	//Main game loop
 	while (window.isOpen()) {
@@ -54,7 +84,7 @@ int main() {
 		}
 	
 		sf::Vector2f oldPosition = player1sprite.getPosition();
-		sf::Vector2f oldPosition1 = enemy1sprite.getPosition();
+		sf::Vector2f oldPosition1 = skeletonSprite.getPosition();
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))	
 			player1sprite.setPosition(oldPosition + sf::Vector2f(0, -1));
 		
@@ -69,27 +99,48 @@ int main() {
 		
 		//Movement For Enemy Sprite
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::I))
-			enemy1sprite.setPosition(oldPosition1 + sf::Vector2f(0, -1));
+			skeletonSprite.setPosition(oldPosition1 + sf::Vector2f(0, -1));
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::J))
-			enemy1sprite.setPosition(oldPosition1 + sf::Vector2f(-1, 0));
+			skeletonSprite.setPosition(oldPosition1 + sf::Vector2f(-1, 0));
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
-			enemy1sprite.setPosition(oldPosition1 + sf::Vector2f(1, 0));
+			skeletonSprite.setPosition(oldPosition1 + sf::Vector2f(1, 0));
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::M))
-			enemy1sprite.setPosition(oldPosition1 + sf::Vector2f(0, 1));
+			skeletonSprite.setPosition(oldPosition1 + sf::Vector2f(0, 1));
+		//
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+
+			Bullets.push_back(sf::RectangleShape(sf::Vector2f(15, 6)));
+
+			int i = Bullets.size() - 1;
+			Bullets[i].setPosition(player1sprite.getPosition());
+
+
+		}
+
+		for (size_t i = 0; i < Bullets.size(); i++) {
+
+			sf::Vector2f bulletDirection = skeletonSprite.getPosition() - Bullets[i].getPosition();
+			bulletDirection = NormalizeVector(bulletDirection);
+
+			Bullets[i].setPosition(Bullets[i].getPosition() + (bulletDirection * bulletSpeed));
+		}
+		
+		
 		//------------ UPDATE -------------
 
 			//------------ DRAW -------------
-			window.clear(sf::Color::Black);
 
+			window.clear(sf::Color::Black);
+			window.draw(skeletonSprite);
 			window.draw(player1sprite);
-			window.draw(enemy1sprite);
+			for (size_t i = 0; i < Bullets.size(); i++) {
+				window.draw(Bullets[i]);
+			}
 
 			window.display();
+
 			//------------ DRAW -------------
-
-		
-
-
 
 
 
